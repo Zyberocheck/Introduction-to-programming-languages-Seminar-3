2076f632a2e98e2aee12017b368f71af3a2fe785
Alexandr <sasha-bragin@mail.ru>
2023-03-06 21:44:58 +0300

    палиндром исправил ошибки и добавил более простой вариант решения

--- Domashnee_Zadanie3/Program.cs
+++ Domashnee_Zadanie3/Program.cs
@@ -6,22 +6,40 @@
 // 23432 -> да
 
 
+// Console.WriteLine("Введите пятизначное число, а я проверю палиндром ли оно: ");
+// int polin = Convert.ToInt32(Console.ReadLine());
+
+// if (polin > 9999 && polin < 100000)
+// {
+//     PolinNumberp(polin);
+// }
+
+// void PolinNumberp(int palin)
+// {
+//     string polin = palin.ToString();
+//     if(polin[0] == polin[4] && polin[1] == polin[3])
+// {
+//        Console.WriteLine(polin + " палиндром!");
+// }else {
+//      Console.WriteLine(polin + " не палиндром!");
+// }
+// }
+
+// Более простой вариант !!!!
+
 Console.WriteLine("Введите пятизначное число, а я проверю палиндром ли оно: ");
-int polin = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 
-if (polin > 9999 && polin < 100000)
+if (a > 9999 && a < 100000)
 {
-    PolinNumberp(polin);
+    String ap = Convert.ToString(a);
+    if (ap[0] == ap[4] && ap[1] == ap[3])
+{
+    Console.WriteLine( a + " палиндром");
+}else{
+    Console.WriteLine( a + " не палиндром");
 }
 
-void PolinNumberp(int palin)
-{
-    string polin = palin.ToString();
-    if(polin[0] == polin[4] && polin[1] == polin[3])
-{
-       Console.WriteLine(polin + " палиндром!");
-}else {
-     Console.WriteLine(polin + " не палиндром!");
+}else{
+    Console.WriteLine( "Не верный ввод");
 }
-}
-
